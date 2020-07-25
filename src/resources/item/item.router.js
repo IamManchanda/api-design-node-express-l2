31bbcef2c4b4a701673c17b91c01a46533d3ffc8
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-25 23:08:20 +0530

    Add exercise solution #done

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,16 @@ import { Router } from "express";
 
 const router = Router();
 
+const mockController = (req, res) => {
+  res.send({ message: "Hello World" });
+};
+
+router.route("/").get(mockController).post(mockController);
+
+router
+  .route("/:id")
+  .get(mockController)
+  .put(mockController)
+  .delete(mockController);
+
 export default router;
